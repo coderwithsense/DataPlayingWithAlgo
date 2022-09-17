a0dd87958656fa5a054f88ce433237b805df7ed8
coderwithsense <102344906+coderwithsense@users.noreply.github.com>
2022-09-17 18:10:49 +0530

    Time: 16 ms (86.16%), Space: 13.3 MB (19.65%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,6 +3,7 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         unordered_map<int, bool> umapi;
         unordered_map<int, bool> umapj;
+//         marking all the rows and columns with 0 as TRUE in umap
         for(int i=0; i<matrix.size(); i++){
             for(int j=0; j<matrix[0].size(); j++){
                 if(matrix[i][j]==0){
@@ -11,6 +12,7 @@ public:
                 }
             }
         }
+//         if the row or column is true then turn it into zero
         for(int i=0; i<matrix.size(); i++){
             for(int j=0; j<matrix[0].size(); j++){
                 if(umapi[i]){
