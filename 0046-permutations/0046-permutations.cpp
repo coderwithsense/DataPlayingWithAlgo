a43501dcb7fcaf49463bbde6454dad912f15db7f
coderwithsense <102344906+coderwithsense@users.noreply.github.com>
2022-11-29 20:00:31 +0530

    Time: 18 ms (5.56%), Space: 9.6 MB (5.20%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,19 +1,26 @@
 class Solution {
 private:
     void solve(vector<vector<int>>& answer, vector<int> ans, vector<int> nums){
-        if(!nums.size()){
+        if(nums.size() == 0){
             answer.push_back(ans);
-            ans = {};
             return;
         }
-            for(int j=0; j<nums.size(); j++){
-                vector<int> temp_nums = nums;
-                vector<int> temp_ans = ans;
-                temp_ans.push_back(temp_nums[j]);
-                auto iterator = temp_nums.begin()+j;
-                temp_nums.erase(iterator);
-                solve(answer, temp_ans, temp_nums);
+        for(int i=0; i<nums.size(); i++){
+            vector<int> temp = nums;
+            vector<int> temp2 = {};
+            vector<int> temp_ans = ans;
+            temp_ans.push_back(temp[i]);
+            for(int j=0; j<temp.size(); j++){
+                if(j != i){
+                    temp2.push_back(temp[j]);
+                }
             }
+            for(int i=0; i<temp2.size(); i++){
+                cout<<temp2[i];
+            }
+            cout<<endl;
+            solve(answer, temp_ans, temp2);
+        }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
